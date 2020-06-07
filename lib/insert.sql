9d415a9677b2d42cd495a2d207ac498d2e5c202a
Shana Wiltshire <shanawiltshire@gmail.com>
2020-06-07 22:25:15 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,2 +1,9 @@
 INSERT INTO series (title, author_id, subgenre_id) VALUES ('Octavia_Butler', 1, 1);
 INSERT INTO series (title, author_id, subgenre_id) VALUES ('James_Baldwin', 2, 2);
+
+INSERT INTO books (title, year, author_id) VALUES ('Wild_Seed', 1980, 1);
+INSERT INTO books (title, year, author_id) VALUES ('Patternmaster', 1976, 1);
+INSERT INTO books (title, year, author_id) VALUES ('Dawn', 1987, 1);
+INSERT INTO books (title, year, author_id) VALUES ('Another_Country', 1962, 2);
+INSERT INTO books (title, year, author_id) VALUES ('Giovannis_Room', 1956, 2);
+INSERT INTO books (title, year, author_id) VALUES ('Sonnys_Blues', 1957, 2);
