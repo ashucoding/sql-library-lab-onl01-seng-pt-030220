ef53365b4b3684bf99b743c9f98e661b5139a23e
Shana Wiltshire <shanawiltshire@gmail.com>
2020-06-07 20:21:50 +0000

    Automatically backed up by Learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -4,4 +4,3 @@ CREATE TABLE authors (id INTEGER PRIMARY KEY, name TEXT);
 CREATE TABLE books (id INTEGER PRIMARY KEY, title TEXT, year INTEGER, series_id INTEGER);
 CREATE TABLE character (id INTEGER PRIMARY KEY, name TEXT, species TEXT, motto TEXT, author_id INTEGER);
 CREATE TABLE character_books (id INTEGER PRIMARY KEY, book_id INTEGER, character_id INTEGER);
-
