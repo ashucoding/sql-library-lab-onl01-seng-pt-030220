1c553e7b86a567c9423c5cca807e9967e3c449f6
Shana Wiltshire <shanawiltshire@gmail.com>
2020-06-07 22:49:34 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -7,3 +7,12 @@ INSERT INTO books (title, year, author_id) VALUES ('Dawn', 1987, 1);
 INSERT INTO books (title, year, author_id) VALUES ('Another_Country', 1962, 2);
 INSERT INTO books (title, year, author_id) VALUES ('Giovannis_Room', 1956, 2);
 INSERT INTO books (title, year, author_id) VALUES ('Sonnys_Blues', 1957, 2);
+
+INSERT INTO characters (name, species, motto, author_id) VALUES ('Doro', 'God', 'Civilization is the way one''s own people live. Savagery is the way foreigners live.', 1);
+INSERT INTO characters (name, species, motto, author_id) VALUES ('Amber', 'human', 'motto_2', 1);
+INSERT INTO characters (name, species, motto, author_id) VALUES ('Lilith', 'God', 'motto_3', 1);
+INSERT INTO characters (name, species, motto, author_id) VALUES ('Rufus', 'human', 'motto_4', 2);
+INSERT INTO characters (name, species, motto, author_id) VALUES ('David', 'human', 'motto_5', 2);
+INSERT INTO characters (name, species, motto, author_id) VALUES ('Sonny', 'human', 'motto_6', 2);
+INSERT INTO characters (name, species, motto, author_id) VALUES ('Aya', 'human', 'motto_7', 1);
+INSERT INTO characters (name, species, motto, author_id) VALUES ('Jacob', 'human', 'motto_8', 2);
